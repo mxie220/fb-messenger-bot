2560319d969974315d2ac231dd7e50143dbbfacf
Michelle Xie <mxie220@aucklanduni.ac.nz>
2017-02-01 22:14:08 +1300

    fixed the spaces

--- app.py
+++ app.py
@@ -135,22 +135,17 @@ def instructions(step):
         return "Open up the app and choose one of the following options:\n\
         1. Go on a date\n\
         2. Meet a new friend\n\
-        3. Network with people\n\n\
-        Message me the number '3' once you have completed."
+        3. Network with people\n\n\Message me the number '3' once you have completed."
     elif step == 3:
-        return "Now choose one of the following options to connect into Scope.\n\n\
-        Message me the number '4' once you have completed."
+        return "Now choose one of the following options to connect into Scope.\n\n\Message me the number '4' once you have completed."
     elif step == 4:
-        return "Take a selfie - make sure it's a good angle.\n\n\
-        Message me the number '5' once you have completed."
+        return "Take a selfie - make sure it's a good angle.\n\n\Message me the number '5' once you have completed."
     elif step == 5:
         return "Set up your profile by choosing:\n\
         1. Who you're interested in\n\
         2. The ages you're interested in\n\
         3. How far you're willing to walk\n\
-        4. Optional: use this only if you're looking for someone w=very specific\n\
-        Then press 'Save'\n\n\
-        Message me the number '6' once you have completed."
+        4. Optional: use this only if you're looking for someone w=very specific\n\Then press 'Save'\n\n\Message me the number '6' once you have completed."
     elif step == 6:
         return "Congratulations! You're now ready to Scope.\n"
     else:
